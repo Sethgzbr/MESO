b206f93bf3fa189f6c9b33de366172409f9417e7
Seth Ghazaryan Barbaryan <113700502+Sethgzbr@users.noreply.github.com>
2024-12-19 11:37:23 +0100

    Update persona.py

--- API/persona.py
+++ API/persona.py
@@ -1,13 +1,9 @@
-def persona_schema(persona) -> dict:
+def persona_schema(reg) -> dict:
     return {
-        "id_persona": persona[0],
-        "nombre": persona[1],
-        "apellido": persona[2],
-        "cargo": persona[3],
-        "departamento": persona[4],
-        "tipo_de_usuario": persona[5],
-        "id_organizacion": persona[6],
+        "fecha": reg[0],
+        "nombre": reg[1],
+        "id_persona": reg[2]
     }
 
 def personas_schema(personas) -> list:
-    return [persona_schema(persona) for persona in personas]
+    return [persona_schema(reg) for reg in personas]
